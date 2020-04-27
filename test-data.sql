91a24e3d1890e19c8a9d17d23ead1770e84013cd
Caleb Caviness <46906713+calebcav@users.noreply.github.com>
2020-04-27 13:34:28 -0700

    The rest of the project lol

--- test-data.sql
+++ test-data.sql
@@ -1,3 +1,4 @@
+/*
 INSERT INTO HOUSE (name, price, address, aid, rent, status)
 VALUES
   (
@@ -12,12 +13,199 @@ VALUES
     'Rock',
     14141,
     'Bikini Bottom',
-    1,
+    3,
     0,
     'pending'
   ),
-  ('Statue', 0, 'Bikini Bottom', 1, 1, 'closed');
+  ('Statue', 0, 'Bikini Bottom', 2, 1, 'closed');
+
 INSERT INTO USER (name, phoneNumber, email)
 VALUES
   ('Caleb', '911', 'lol@hotmail.com'),
   ('Bryan', '14904190', 'naona@yahoo.com');
+INSERT INTO AGENT (name, phoneNumber, email)
+VALUES('Spongebob', '02824', 'spongebob@fb.com'),
+  ('Squidward', '10841', 'squidward@google.com'),
+  ('Patrick', '0', 'patrick@apple.com');
+
+INSERT INTO AGENT (name, phoneNumber, email)
+VALUES('Elisee', '5147832494', 'elprofessor@yahoo.com'),
+  ('Jose', '7908743210', 'gamer123@gmail.com'),
+  (
+    'Caleb',
+    '6204206969',
+    'XxxnotcalebxxX@gmail.com'
+  ),
+  ('Tariq', '3133456750', 'c00ldude@gmail.com'),
+  ('Jenn', '8704578792', 'jennjen@gmail.com');
+INSERT INTO USER (name, phoneNumber, email)
+VALUES('Kelsi', '9152072343', 'kfulton@gmail.com'),
+  ('Mary Jo', '9753123342', 'mjthebest@gmail.com'),
+  (
+    'Obama',
+    '9754114042',
+    'thepresident50@gmail.com'
+  ),
+  ('Sally', '4204347343', 'mlteacher@gmail.com'),
+  ('Diego', '4203434323', 'youngman@gmail.com'),
+  ('Marco', '7343245708', 'thatTA@yahoo.com'),
+  ('Juan', '8207485804', 'algrman@gmail.com'),
+  ('Daniel', '9134321343', 'musicalML@gmail.com'),
+  ('Kevin', '9183483433', 'bluebirbs@gmail.com'),
+  ('Kousei', '1234567890', 'jigglyrest@yahoo.com'),
+  ('Jean', '2113453234', 'thebestofbest@gmail.com'),
+  (
+    'Kaushik',
+    '3423438234',
+    'alwayscurious@yahoo.com'
+  ),
+  ('Tanjin', '3223384330', 'prity.tan@gmail.com'),
+  (
+    'Kerry',
+    '4044033483',
+    'stopaskingquestions@gmail.com'
+  ),
+  ('Lauren', '2304889215', 'confusedart@yahoo.com');
+*/
+/*
+INSERT INTO HOUSE (name, price, address, aid, rent, status, city)
+VALUES(
+    'Breeze Home',
+    120350,
+    '123 Kennedy Drive',
+    1,
+    1,
+    'active',
+    'Los Angeles'
+  ),
+  (
+    'Le Atelier',
+    620,
+    '320 Clinton Avenue',
+    2,
+    1,
+    'pending',
+    'Los Angeles'
+  ),
+  (
+    'White House',
+    550,
+    '1600 Pennsylvania Ave',
+    3,
+    1,
+    'closed',
+    'Washington D.C.'
+  ),
+  (
+    'Lunar Door',
+    10000,
+    '618 Martin Luther King Street',
+    1,
+    1,
+    'active',
+    'Los Angeles'
+  ),
+  (
+    'Masquerade',
+    2000,
+    '711 Lakewood Street',
+    1,
+    1,
+    'closed',
+    'Seattle'
+  ),
+  (
+    'The Chief',
+    250000,
+    '903 Michigan Road',
+    4,
+    0,
+    'active',
+    'Seattle'
+  ),
+  (
+    'Sandy Dome',
+    10000,
+    '127 Bikini Bottom Drive',
+    3,
+    0,
+    'active',
+    'Bikini Bottom'
+  ),
+  (
+    'Krab Anchor',
+    939942,
+    '811 Rich Boulevard',
+    2,
+    0,
+    'active',
+    'Bikini Bottom'
+  ),
+  (
+    'Sherlock Home',
+    700543,
+    '682 East Evelyn Avenue',
+    5,
+    0,
+    'active',
+    'London'
+  ),
+  (
+    'Sponge Pineapple',
+    1000000,
+    '414 Annoying Avenue',
+    3,
+    0,
+    'active',
+    'Bikini Bottom'
+  ),
+  (
+    'Patrick Rock',
+    169000,
+    '412 Annoying Avenue',
+    3,
+    0,
+    'active',
+    'Bikini Bottom'
+  ),
+  (
+    'Squidward Statue',
+    182900,
+    '413 Annoying Avenue',
+    2,
+    0,
+    'pending',
+    'Bikini Bottom'
+  ),
+  (
+    'Plankton Bucket',
+    416000,
+    '524 Poor Road',
+    4,
+    0,
+    'coming soon',
+    'Bikini Bottom'
+  );
+  /*
+        INSERT INTO PURCHASE (uid, hid, date_of_purchase)
+        VALUES(3, 14, '2008-15-01'),
+          (5, 15, '2012-04-05'),
+          (13, 13, '2015-08-10'),
+          (15, 16, '2019-01-08');
+        */
+INSERT INTO INTERESTS (uid, hid)
+VALUES(1, 23),
+  (2, 23),
+  (3, 24),
+  (4, 25),
+  (5, 26),
+  (6, 27),
+  (7, 28),
+  (8, 25),
+  (9, 29),
+  (10, 30),
+  (11, 31),
+  (12, 32),
+  (13, 34),
+  (13, 23),
+  (1, 30);
--- voyager/views/houses.py
+++ voyager/views/houses.py
@@ -12,28 +12,75 @@ def getAllHouses(conn):
     return execute(conn, "SELECT * FROM HOUSE")
 
 
+def getAllAgents(conn):
+    return execute(conn, "SELECT a.name, a.phoneNumber, a.email FROM AGENT AS a")
+
+
+def getAllUsers(conn):
+    return execute(conn, "SELECT * FROM USER")
+
+
 def getAllHousesByAgent(conn, name):
-    pass
+
+    return execute(conn, f"SELECT h.name, h.price, h.address FROM HOUSE AS h INNER JOIN AGENT AS a ON h.aid = a.aid WHERE a.name='{name}'")
 
 
 def getAllHousesInterested(conn, userName):
-    pass
+
+    return execute(conn, f"SELECT h.name FROM HOUSE AS h INNER JOIN User AS u INNER JOIN Interests as i on u.id = i.id WHERE u.name = '{userName}'")
 
 
-def addHouse(conn, name, price, address, aid, rent, status):
-    pass
+def addHouse(conn, name, price, address, aid, rent, status, city):
+
+    return execute(conn, f"INSERT INTO House(name, price, address, aid, rent, status, city) VALUES ('{name}', {price}, '{address}', {aid}, {rent}, '{status}', '{city}')")
 
 
-def filteredSearchForHouses(conn, name, maxPrice, address, aid, rent, status):
-    pass
+def filteredSearchForHouses(conn, maxPrice, city, aid, rent):
+    if aid:
+        return execute(conn, f"SELECT h.name, h.price, h.address FROM House as h WHERE h.price <=  {maxPrice}  AND h.city= '{city}' AND h.aid= {aid} AND h.rent= {rent} AND h.status= 'active'")
+    else:
+        return execute(conn, f"SELECT h.name, h.price, h.address FROM House as h WHERE h.price <=  {maxPrice}  AND h.city= '{city}' AND h.rent= {rent} AND h.status= 'active'")
 
 
 def addInterest(conn, userid, houseid):
-    pass
+
+    return execute(conn, f"INSERT INTO Interests(uid, hid) VALUES ({userid}, {houseid})")
 
 
 def getMostPopularHouse(conn):
-    pass
+    return execute(conn, "SELECT h.name, count(*) FROM House AS h INNER JOIN Interests AS i ON h.hid=i.hid GROUP BY h.name ORDER BY count(*) DESC")
+
+
+def findAgentByName(conn, name):
+    return execute(conn, f"SELECT * FROM AGENT WHERE name = '{name}'")
+
+
+def findUserByName(conn, name):
+    return execute(conn, f"SELECT * FROM USER WHERE name = '{name}'")
+
+
+def findHouseByName(conn, name):
+    return execute(conn, f"SELECT * FROM HOUSE WHERE name = '{name}'")
+
+
+def currentDate(conn):
+    return execute(conn, "SELECT CURRENT_TIMESTAMP")
+
+
+def addPurchase(conn, uid, hid, date):
+    return execute(conn, f"INSERT INTO PURCHASE(uid, hid, date_of_purchase) VALUES({uid}, {hid}, '{date}')")
+
+
+def updateHouse(conn, hid):
+    return execute(conn, f"UPDATE HOUSE SET status='closed' WHERE hid = {hid}")
+
+
+def addUser(conn, name, number, email):
+    return execute(conn, f"INSERT INTO USER(name, phoneNumber, email) VALUES('{name}', {number}, '{email}')")
+
+
+def findPurchaseByUser(conn, uid):
+    return execute(conn, f"SELECT p.date_of_purchase, h.name FROM PURCHASE as p INNER JOIN HOUSE as h ON h.hid = p.hid WHERE p.uid = {uid}")
 
 
 def views(bp):
@@ -46,4 +93,143 @@ def views(bp):
     @bp.route("/agent-houses", methods=["POST", "GET"])
     def all_agent_houses():
 
-        return request.form["agent-name"]
+        with get_db() as conn:
+            agent_name = request.form['agent-name']
+            rows = getAllHousesByAgent(conn, agent_name)
+
+        return render_template("table.html", name=f"Houses by {agent_name}", rows=rows)
+
+    @bp.route("/search-houses", methods=["POST", "GET"])
+    def house_search():
+
+        if request.method == "POST":
+
+            with get_db() as conn:
+                price = request.form['price']
+                city = request.form['city']
+                try:
+                    agentName = request.form['agent']
+                    agent_id = (findAgentByName(conn, agentName))[0]['aid']
+                except:
+                    agent_id = None
+                if request.form.get('rent'):
+                    rent = request.form.get('rent')
+                else:
+                    rent = 0
+                row = filteredSearchForHouses(
+                    conn, price, city, agent_id, rent)
+                print(rent)
+            return render_template("searchhouses.html", name=f"Houses you searched for", rows=row)
+
+    @bp.route("/add-interests")
+    def addInterestsPage():
+        return render_template("addinterests.html")
+
+    @bp.route("/add-interests", methods=["POST", "GET"])
+    def addInterests():
+        if request.method == "POST":
+
+            with get_db() as conn:
+                name = request.form['name']
+                housename = request.form['house']
+
+                house_id = (findHouseByName(conn, housename))[0]['hid']
+                user_id = (findUserByName(conn, name))[0]['uid']
+                addInterest(conn, user_id, house_id)
+
+                return "SUCCESS"
+
+    @bp.route("/all-agents")
+    def all_agents():
+
+        with get_db() as conn:
+            rows = getAllAgents(conn)
+
+        return render_template("table.html", name="List of Agents", rows=rows)
+
+    @bp.route("/add-house")
+    def add_houses_page():
+        return render_template("addhouses.html")
+
+    @bp.route("/add-house", methods=["POST", "GET"])
+    def add_house():
+        if request.method == "POST":
+
+            with get_db() as conn:
+                house_name = request.form['name']
+                house_price = request.form['price']
+                house_address = request.form['address']
+                house_agent_name = request.form['agent']
+                house_status = request.form['status']
+                house_city = request.form['city']
+                try:
+                    house_rent = request.form['rent']
+                except:
+                    house_rent = 0
+
+                agent_id = (findAgentByName(conn, house_agent_name))[0]['aid']
+                if (len(findHouseByName(conn, house_name)) >= 1):
+                    return "Choose a different name for the house"
+
+                addHouse(conn, house_name, house_price, house_address,
+                         agent_id, house_rent, house_status, house_city)
+                return "SUCCESS"
+
+    @bp.route("/most-popular-houses", methods=["POST", "GET"])
+    def pop_houses():
+        with get_db() as conn:
+            rows = getMostPopularHouse(conn)
+            print(currentDate(conn)[0]['CURRENT_TIMESTAMP'].split()[0])
+            return render_template("table.html", rows=rows, name="Most Popular Houses")
+
+    @bp.route("/all-users")
+    def all_users():
+        with get_db() as conn:
+            rows = getAllUsers(conn)
+            return render_template("table.html", rows=rows, name="All users")
+
+    @bp.route("/add-purchase")
+    def purchase_page():
+        return render_template('addpurchase.html')
+
+    @bp.route("/add-purchase", methods=["POST", "GET"])
+    def add_purchase():
+        if request.method == "POST":
+            with get_db() as conn:
+                houseName = request.form['house']
+                userName = request.form['name']
+                uid = findUserByName(conn, userName)[0]['uid']
+                hid = findHouseByName(conn, houseName)[0]['hid']
+                date = currentDate(conn)[0]['CURRENT_TIMESTAMP'].split()[0]
+                if findHouseByName(conn, houseName)[0]['status'] != 'active':
+                    return "House has already been bought"
+
+                addPurchase(conn, uid, hid, date)
+                updateHouse(conn, hid)
+                return "SUCCESS"
+
+    @bp.route("/add-user")
+    def user_page():
+        return render_template('adduser.html')
+
+    @bp.route("/add-user", methods=["POST", "GET"])
+    def add_user():
+        if request.method == "POST":
+            with get_db() as conn:
+                userName = request.form['name']
+                email = request.form['email']
+                phone = request.form['phone-number']
+                if (len(findUserByName(conn, userName)) >= 1):
+                    return "Someone already has that name"
+
+                addUser(conn, userName, phone, email)
+                return "SUCCESS"
+
+    @bp.route("/user-purchases", methods=["POST", "GET"])
+    def user_purchase():
+        if request.method == "POST":
+            with get_db() as conn:
+                userName = request.form['user-name']
+                uid = findUserByName(conn, userName)[0]['uid']
+                rows = findPurchaseByUser(conn, uid)
+                return render_template("table.html", rows=rows, name=f"Purchases by {userName}")
